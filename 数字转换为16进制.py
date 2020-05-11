5fa773171ac2e1d05b6a9d5df9ae3423df3d01e8
leqi <math_leqi@163.com>
2020-05-11 10:35:57 +0800

    添加作答

--- 数字转换为16进制.py
+++ 数字转换为16进制.py
@@ -1,8 +1,30 @@
 class Solution:
     def toHex(self, num: int) -> str:
-        pass
+        if num >= 0:
+            if num == 0:
+                return '0'
+            d={
+                10:'a',
+                11:'b',
+                12:'c',
+                13:'d',
+                14:'e',
+                15:'f'
+            }
+            ret = ''
+            while num:
+                t = num % 16
+                t1 = d.get(t)
+                n = t1 if t1 else str(t)
+                ret = n+ret
+                num = num // 16
+            return ret
+        else:
+            return self.toHex(2**32+num)
+        
 
 if __name__ == "__main__":
     a = Solution()
     print(a.toHex(26))
+    print(a.toHex(17))
     print(a.toHex(-1))
